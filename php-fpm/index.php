4f5be9cc1c84fad675ee1c773dd9c870a0c9399e
Guenther Froestl <gfr@gammadata.de>
2020-10-17 20:21:37 +0200

    got a phpinfo() example working

---
+++ php-fpm/index.php
@@ -0,0 +1,7 @@
+<?php
+//Document root
+printf ('--------------------- Start print infos  -------------------------------------------------------------------------%1$s', PHP_EOL);
+// Zeigt alle Informationen (Standardwert ist INFO_ALL)
+phpinfo();
+printf ('--------------------- End print infos  -------------------------------------------------------------------------%1$s', PHP_EOL);
+?>
